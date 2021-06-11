7f41489eedbd040cc4094432b60e64ca8280fc50
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2021-06-11 18:33:39 +0300

    Implemented advanced panel for equal album page

--- src/Components/Albums/Albums.tsx
+++ src/Components/Albums/Albums.tsx
@@ -4,6 +4,7 @@ import { Formik } from "formik"
 import Menu from "@material-ui/core/Menu"
 import type { Components } from "./../../types"
 import classes from "./../../constants/AlbumPage/Albums.module.css"
+import advancedPanelButton from "./../../constants/AdvancedPanelButton/AdvancedPanelButton.module.css"
 import errorClasses from "./../../constants/Errors/Errors.module.css"
 import MediaOrderer from "./../MediaOrderer/MediaOrderer"
 import Preloader from "./../Preloader/Preloader"
@@ -22,75 +23,31 @@ const AdvancePanel = (props: Components.Albums.AdvancePanelType) => {
         <div className={classes["window"]}>
             <Menu className={classes["block"]} open={Boolean(anchorEl)} keepMounted anchorEl={anchorEl} >
                 <img className={classes["close-button"]} onClick={() => setAnchorEl(null)} src="https://img.icons8.com/metro/26/000000/multiply.png" />
-                <div>
-                    <div className={classes["group1"]}>
-                        <Formik
-                            initialValues={{ albumName: "" }}
-                            validate={(values) => {
-                                if (values.albumName.length == 0) {
-                                    return { albumName: "Album name is required" }
-                                }
-                            }}
-                            onSubmit={(values, actions) => {
-                                props.handleFormCreate(values.albumName)
-                                actions.resetForm()
-                            }}
-                        >
-                            {({ errors, values, handleChange, handleSubmit }) => (
-                                <div className={classes["form"]}>
-                                    <label htmlFor="create">Album name</label>
-                                    <Input onChange={handleChange} id="create" type="text" name="albumName" placeholder="ex: Summer on the beach" />
-                                    <span className={errorClasses["error"]}>{errors.albumName}</span>
-                                    <Button color={"primary"} onClick={() => handleSubmit()} type="button">Create an album</Button>
-                                </div>
-                            )}
-                        </Formik>
-                    </div>
-                    <div className={classes["group2"]}>
-                        <Formik
-                            initialValues={{ albumName: "", directories: [] as any, files: [] as any }}
-                            validate={(values) => {
-
-                                if (values.albumName.length == 0) {
-                                    return { albumName: "Album name is required" }
-                                }
-                                if (props.albumsPage.result) {
-                                    const albumExists = (): boolean => {
-                                        for (const i of props.albumsPage.result) {
-                                            if (i.name == values.albumName) return true
-                                        }
-                                        return false
-                                    }
-                                    if (!albumExists()) {
-                                        return { albumName: "Such album does not exist" }
-                                    }
-                                }
-
-                            }}
-                            onSubmit={(values, actions) => {
-                                props.handleFormAdd(values.albumName, values.directories, values.files)
-                                actions.resetForm()
-                            }}
-                        >
-                            {({ errors, values, handleChange, handleSubmit, setFieldValue }) => (
-                                <div className={classes["form"]}>
-                                    <label htmlFor="albumName">Album name</label>
-                                    <Input onChange={handleChange} type="text" name="albumName" id="albumName" placeholder="ex: Chernobyl trip" />
-                                    <span className={errorClasses["error"]}>{errors.albumName}</span>
-                                    <label htmlFor="directories">Add directroy</label>
-                                    <input hidden ref={directoryUploader} onChange={(e) => setFieldValue("directories", e.currentTarget.files)} type="file" name="directories" id="directories" directory="" webkitdirectory="" />
-                                    <label htmlFor="directories"><Button startIcon={<UploadIcon />} onClick={() => directoryUploader.current?.click()} variant={"contained"}>Upload</Button></label>
-                                    <label htmlFor="files">Add photos</label>
-                                    <input hidden ref={multipleUploader} onChange={(e: any) => setFieldValue("files", e.currentTarget.files)} type="file" name="files" id="files" accept="image/jpeg;image/png;image/jpg" multiple />
-                                    <label htmlFor="files"><Button startIcon={<UploadIcon />} onClick={() => multipleUploader.current?.click()} variant={"contained"}>Upload</Button></label>
-                                    <Button color={"primary"} onClick={() => handleSubmit()} type="button">Download photos</Button>
-                                </div>
-                            )}
-                        </Formik>
-                    </div>
+                <div className={classes["group1"]}>
+                    <Formik
+                        initialValues={{ albumName: "" }}
+                        validate={(values) => {
+                            if (values.albumName.length == 0) {
+                                return { albumName: "Album name is required" }
+                            }
+                        }}
+                        onSubmit={(values, actions) => {
+                            props.handleFormCreate(values.albumName)
+                            actions.resetForm()
+                        }}
+                    >
+                        {({ errors, values, handleChange, handleSubmit }) => (
+                            <div className={classes["form"]}>
+                                <label htmlFor="create">Album name</label>
+                                <Input onChange={handleChange} id="create" type="text" name="albumName" placeholder="ex: Summer on the beach" />
+                                <span className={errorClasses["error"]}>{errors.albumName}</span>
+                                <Button color={"primary"} onClick={() => handleSubmit()} type="button">Create an album</Button>
+                            </div>
+                        )}
+                    </Formik>
                 </div>
             </Menu>
-            <img onClick={(e) => setAnchorEl(e.currentTarget)} className={classes["block-button"]} src="https://img.icons8.com/material-sharp/24/000000/plus--v1.png" />
+            <img onClick={(e) => setAnchorEl(e.currentTarget)} className={advancedPanelButton["button"]} src="https://img.icons8.com/material-sharp/24/000000/plus--v1.png" />
         </div >
     )
 }
--- src/Components/EqualAlbum/EqualAlbum.tsx
+++ src/Components/EqualAlbum/EqualAlbum.tsx
@@ -1,22 +1,72 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import Menu from "@material-ui/core/Menu"
+import Input from "@material-ui/core/Input"
 import { Redirect } from "react-router-dom"
 import type { Components } from "./../../types"
 import Button from "@material-ui/core/Button"
+import UploadIcon from "@material-ui/icons/CloudUpload"
 import Tooltip from "@material-ui/core/Tooltip"
 import { Formik } from "formik"
-import classes from "./../../constants/EqualAlbumPage/EqualAlbum.module.css"
+import classes from "./../../constants/EqualAlbum/EqualAlbum.module.css"
+import advancedPanelButton from "./../../constants/AdvancedPanelButton/AdvancedPanelButton.module.css"
 import errorClasses from "./../../constants/Errors/Errors.module.css"
 import MediaOrderer from "./../MediaOrderer/MediaOrderer"
-import KebabMenu from "./../../assets/images/menukebab.png"
 
 const AdvancePanel = (props: Components.EqualAlbum.AdvancedPanelType) => {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const multipleUploader = useRef<HTMLInputElement>(null)
+    const directoryUploader = useRef<HTMLInputElement>(null)
+
     return (
-        <div className={classes["panel"]} >
-            <Tooltip title={"Deletes opened album"}>
-                <Button onClick={() => props.handleDeleteAlbum(props.name)} color={"primary"}>Delete</Button>
-            </Tooltip >
-        </div>
+        <div>
+            <div className={classes["window"]}>
+                <Menu className={classes["block"]} open={Boolean(anchorEl)} keepMounted anchorEl={anchorEl} >
+                    <img className={classes["close-button"]} onClick={() => setAnchorEl(null)} src="https://img.icons8.com/metro/26/000000/multiply.png" />
+                    <div className={classes["group"]}>
+                        <Formik
+                            initialValues={{ directories: [] as any, files: [] as any }}
+                            validate={(values) => {
+                                if (values.directories.length == 0) {
+                                    return { directories: "You should add media" }
+                                }
+                                if (values.files.length == 0) {
+                                    return { files: "You should add media" }
+                                }
+                            }}
+                            onSubmit={(values, actions) => {
+                                actions.resetForm()
+                            }}
+                        >
+                            {({ errors, handleSubmit, setFieldValue }) => (
+                                <div className={classes["form"]}>
+                                    <label htmlFor="directories">Add directroy</label>
+                                    <input hidden ref={directoryUploader} onChange={(e) => setFieldValue("directories", e.currentTarget.files)} type="file" name="directories" id="directories" directory="" webkitdirectory="" />
+                                    <span className={errorClasses["error"]}>{errors.directories}</span>
+                                    <label htmlFor="directories"><Button startIcon={<UploadIcon />} onClick={() => directoryUploader.current?.click()} variant={"contained"}>Upload</Button></label>
+                                    <label htmlFor="files">Add photos</label>
+                                    <input hidden ref={multipleUploader} onChange={(e: any) => setFieldValue("files", e.currentTarget.files)} type="file" name="files" id="files" accept="image/jpeg;image/png;image/jpg" multiple />
+                                    <span className={errorClasses["error"]}>{errors.files}</span>
+                                    <label htmlFor="files"><Button startIcon={<UploadIcon />} onClick={() => multipleUploader.current?.click()} variant={"contained"}>Upload</Button></label>
+                                    <Button variant={"outlined"} color={"primary"} onClick={() => handleSubmit()} type="button">Download photos</Button>
+                                </div>
+                            )}
+                        </Formik>
+                    </div>
+                    <div className={classes["options"]}>
+                        <Button color={"secondary"}>Delete</Button>
+                    </div>
+                </Menu>
+            </div>
+            <img onClick={(e) => setAnchorEl(e.currentTarget)} className={advancedPanelButton["button"]} src="https://img.icons8.com/material-sharp/24/000000/plus--v1.png" />
+        </div >
     )
+    // return (
+    //     // <div className={classes["panel"]} >
+    //     //     <Tooltip title={"Deletes opened album"}>
+    //     //         <Button onClick={() => props.handleDeleteAlbum(props.name)} color={"primary"}>Delete</Button>
+    //     //     </Tooltip >
+    //     // </div>
+    // )
 }
 
 const EqualAlbum = (props: Components.EqualAlbum.EqualAlbumType) => {
